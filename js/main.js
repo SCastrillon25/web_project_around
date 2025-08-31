212a9e7f89dc7481c9302f7bdf357997da9f7608
SCastrillon25 <secastrillonos@gmail.com>
2025-08-31 13:51:22 -0500

    modalEditProfile

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const buttonEdit = document.querySelector(".profile__edit-button");
+const containerModalEdit = document.querySelector(".modal-edit-container");
+const formModalEdit = document.querySelector(".modal-edit__form");
+
+
+const buttonCloseModalEdit = document.querySelector(".modal-edit__button-close");
+const nameInput = document.querySelector(".modal-edit__input-name");
+const descriptionInput = document.querySelector(".modal-edit__input-description");
+const nameProfile = document.querySelector(".profile__name");
+const descriptionProfile = document.querySelector(".profile__description");
+
+
+
+
+function closeModal() {
+   containerModalEdit.classList.remove("modal-edit-open");
+}
+
+function saveProfileSubmit(evt) {
+   evt.preventDefault();
+
+   nameProfile.textContent = nameInput.value;
+   descriptionProfile.textContent = descriptionInput.value;
+
+   closeModal();
+}
+
+function openModal() {
+   containerModalEdit.classList.add("modal-edit-open");
+   nameInput.value = nameProfile.textContent;
+   descriptionInput.value = descriptionProfile.textContent;
+}
+
+
+buttonEdit.addEventListener("click", openModal);
+formModalEdit.addEventListener("submit", saveProfileSubmit);
+buttonCloseModalEdit.addEventListener("click", closeModal);
